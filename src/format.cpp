976eab5b96493c4d760b875c784c7152444d52fd
milan-r-shah <mrshah@wpi.edu>
2020-03-26 19:52:31 -0400

    Format.cpp: Removed [[maybe_unused]] from string Format::ElapsedTime

--- src/format.cpp
+++ src/format.cpp
@@ -8,7 +8,7 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) {
+string Format::ElapsedTime(long seconds) {
   std::string hr = "";
   std::string min = "";
   std::string sec = "";
