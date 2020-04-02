a06362c89246bb210e415123a9036b7a20a059ea
milan-r-shah <mrshah@wpi.edu>
2020-04-02 01:52:13 -0400

    Temporary commit!

--- src/process.cpp
+++ src/process.cpp
@@ -1,23 +1,68 @@
+#include "process.h"
+
 #include <unistd.h>
+
 #include <cctype>
 #include <sstream>
 #include <string>
 #include <vector>
 
-#include "process.h"
 #include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
-Process::Process (int pid) : pid_(pid) {}
+Process::Process(int pid) : pid_(pid) {}
 
 // TODO: Return this process's ID
 int Process::Pid() { return pid_; }
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return LinuxParser::ProcessCpuUtil(Pid()); }
+float Process::CpuUtilization() const {
+  // return LinuxParser::ProcessCpuUtil(Pid());
+  float processCpuUtil = 0.0;
+  long upTime = LinuxParser::UpTime();
+
+  std::string line;
+  std::string key, value;
+  int tempPid = Pid();
+  std::string tempString = std::to_string(tempPid);
+
+//   std::ifstream filestream(LinuxParser::kProcDirectory + std::to_string(tempPid) +
+//                            LinuxParser::kStatFilename);
+  std::ifstream filestream(LinuxParser::kProcDirectory + tempString +
+                           LinuxParser::kStatFilename);
+
+  int uTimeClkTks = 0;
+  int sTimeClkTks = 0;
+  int cuTimeClkTks = 0;
+  int csTimeClkTks = 0;
+  float startTimeClkTks = 0.0;
+
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::replace(line.begin(), line.end(), '(', '_');
+      std::replace(line.begin(), line.end(), ')', '_');
+      std::replace(line.begin(), line.end(), '-', '_');
+      std::istringstream linestream(line);
+      std::istream_iterator<std::string> beg(linestream), end;
+      std::vector<std::string> vec(beg, end);
+      uTimeClkTks = std::stoi(vec[13]);
+      sTimeClkTks = std::stoi(vec[14]);
+      cuTimeClkTks = std::stoi(vec[15]);
+      csTimeClkTks = std::stoi(vec[16]);
+      startTimeClkTks = std::stof(vec[21]);
+
+      float totalTime =
+          (float)(uTimeClkTks + sTimeClkTks + cuTimeClkTks + csTimeClkTks);
+      float seconds = upTime - startTimeClkTks / sysconf(_SC_CLK_TCK);
+      processCpuUtil = 100 * ((totalTime / sysconf(_SC_CLK_TCK)) / seconds);
+    }
+  }
+
+  return processCpuUtil;
+}
 
 // TODO: Return the command that generated this process
 string Process::Command() { return LinuxParser::Command(Pid()); }
@@ -33,4 +78,6 @@ long int Process::UpTime() { return LinuxParser::UpTime(Pid()); }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+bool Process::operator<(Process const& a [[maybe_unused]]) const {
+  return true;
+}
